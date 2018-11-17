0dc97ea5d0c4535979fdfe8fe0af85e407b2820f
Janine Luk <janineluk95@gmail.com>
2018-11-17 17:27:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,3 +27,16 @@ def word_substituter(tweet)
   end
   words.join(" ")  
 end
+
+def word_substituter(tweet)
+  dict = dictionary
+  words = tweet.split(" ")
+   words.collect do |word|
+    dictionary.collect do |key, value|
+      if word == key
+        word.replace(value)
+      end
+    end
+  end
+  words.join(" ")
+end
