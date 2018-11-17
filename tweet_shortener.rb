1df6291578b4f1d48c0bc9f74e93aaf27c4f487f
Janine Luk <janineluk95@gmail.com>
2018-11-17 17:17:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def dictionary{
+  :words = {
+    "hello" => "hi",
+    "to, two, too" => 2,
+    "for, four" => 4,
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+}
+  
+end
